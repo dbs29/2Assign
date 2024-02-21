e51204cf1748a8985f8e665c37bf73b00ea1f6cd
dbs29 <69994269+dbs29@users.noreply.github.com>
2024-02-21 15:08:02 -0500

    Update movie_db.py

--- movie_db.py
+++ movie_db.py
@@ -43,7 +43,10 @@ class Movie_db(object):
 
     def q2(self):
         query = '''
-            
+            SELECT m.title, m.year
+            FROM Movies as m, (SELECT *
+                                FROM Moies as rw WHERE rw.title = 'Rogue One: A Star Wars Story') as mRW
+                                WHERE m.year = mRW.year AND m.rank > mRW.rank ORDER BY m.title ASC
         '''
         self.cur.execute(query)
         all_rows = self.cur.fetchall()
@@ -51,7 +54,10 @@ class Movie_db(object):
 
     def q3(self):
         query = '''
-            
+            SELECT fname, lname, mr.creds
+            FROM Actors as act, (Select c.aid, count(role) as cred FROM Moives as m, Cast as c WHERE m.mid = c.mid AND m.title LIKE '%Star Wars%' GROUP BY c.aid Having cred >=1) mr
+            WHERE act.aid = mr.aid
+            ORDER By mr.creds DESC, a.lname ASC, a.fname ASC
         '''
         self.cur.execute(query)
         all_rows = self.cur.fetchall()
